f6d7e3b47b2a24259e10f891b0f667c6b50709e2
DaltonHart <daltonvaren@gmail.com>
2018-08-08 11:43:57 -0700

    converted over to hobbit will add more quotes

--- generator.js
+++ generator.js
@@ -8,10 +8,22 @@ function GenerateNewText() {
   // Add property to the object
   this.sentences =
     [
-      "Awesome quote number 1.",
-      "Second awesome quote.",
-      "Yet another great quote.",
-      "Ok, last quote."
+      "It does not do to leave a live dragon out of your calculations, if you live near him.",
+      "May the wind under your wings bear you where the sun sails and the moon walks.",
+      "Where there's life there's hope.",
+      "So comes snow after fire, and even dragons have their endings.",
+      '“Good Morning!" said Bilbo, and he meant it. ',
+      '"What do you mean?" he said.',
+      '"You have nice manners for a thief and a liar," said the dragon."',
+      '"May the hair on your toes never fall out!"',
+      'A box without hinges, key, or lid, Yet golden treasure inside is hid.',
+      'A safe fairyland is untrue to all worlds.',
+      'There is nothing like looking, if you want to find something.',
+      'Trolls simply detest the very sight of dwarves (uncooked).',
+      '"Escaping goblins to be caught by wolves!" he said.',
+      'Is it nice, my preciousss?',
+      'Is it juicy?',
+      'Is it scrumptiously crunchable?',
    ];
 }
 
